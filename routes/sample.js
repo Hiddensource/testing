1e280892cfca7448519f3875de8ae76425f69f75
Deepak <deepak00704@gmail.com>
2018-12-26 18:00:11 +0530

    Update sample.js

--- routes/sample.js
+++ routes/sample.js
@@ -6,8 +6,11 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   console.log('hit');
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.send({"a":`RESTful API ${req.ip},  ${res.ip}`})
+  //res.send({"a":`RESTful API ${req.ip},  ${res.ip}`})
 // res.status(200).send(`{requestIp.getClientIp(req)} , ${ip}`);  
+   res.send({
+    "a": `RESTful API ${req.ip},  ${ip} , ${requestIp.getClientIp(req)}`
+  })
 });
 
 module.exports = router;
